1682a2e49dd063b0e059a52c2c314cad67e3aaff
frreri <fredrik-eriksson@outlook.com>
2025-04-16 14:08:37 +0200

    removed dist

--- dist/vykort.09c24910.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-const randomNum = (min, max)=>Math.floor(Math.random() * (max - min) + min);
-setInterval(()=>{
-    document.body.style.backgroundColor = `rgb(${randomNum(0, 255)},${randomNum(0, 255)},${randomNum(0, 255)})`;
-}, 1000);
-
-//# sourceMappingURL=vykort.09c24910.js.map
--- dist/vykort.e6dd4867.js
+++
@@ -1,2 +0,0 @@
-"use strict";let randomNum=(o,r)=>Math.floor(Math.random()*(r-o)+o);setInterval(()=>{document.body.style.backgroundColor=`rgb(${randomNum(0,255)},${randomNum(0,255)},${randomNum(0,255)})`},1e3);
-//# sourceMappingURL=vykort.e6dd4867.js.map
